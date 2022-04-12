edb0b114a0ea386719186746dc1c78090479e5ac
armutyus <ofdelibas@gmail.com>
2022-04-12 15:40:44 +0300

    removed unnecessary codes

--- app/src/main/java/com/armutyus/videogamesproject/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/armutyus/videogamesproject/viewmodel/DetailsViewModel.kt
@@ -1,14 +1,11 @@
 package com.armutyus.videogamesproject.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.armutyus.videogamesproject.repo.VideoGamesRepoInterface
 import com.armutyus.videogamesproject.roomdb.Games
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,18 +14,6 @@ class DetailsViewModel @Inject constructor(
     private val repoInterface: VideoGamesRepoInterface
 ) : ViewModel() {
 
-    private val videoGamesDetails = MutableLiveData<Games>()
-    val videoGamesDetailsById: LiveData<Games>
-        get() = videoGamesDetails
-
     fun updateGames(games: Games) =
         CoroutineScope(Dispatchers.IO).launch { repoInterface.updateGames(games) }
-
-    fun getGamesDetailsById(id: Int) {
-        CoroutineScope(Dispatchers.IO).launch {
-            repoInterface.getGamesByIdRoom(id).collectLatest {
-                videoGamesDetails.postValue(it)
-            }
-        }
-    }
 }
